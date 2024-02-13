47df2b60ba5f5dca533ec848a280293c2eaf69fd
Alex <aleksandrekiknadzee@gmail.com>
2024-02-13 11:18:14 +0400

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import {Routes, Route, Link, Outlet} from 'react-router-dom';
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Hi test</h1>
+      <Routes>
+        <Route></Route>
+      </Routes>
+      </div>
+  )
+}
+
+export default App
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+
+const Home = () => {
+
+
+    return (
+        <div>
+            <h2>Hi</h2>
+        </div>
+    )
+}
+
+export default Home;
