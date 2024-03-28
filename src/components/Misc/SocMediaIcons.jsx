d3943d434f3c50af6c62034b4568ad056990fec4
Alex <aleksandrekiknadzee@gmail.com>
2024-03-28 11:46:52 +0400

    feat: add soc media icons component

---
+++ src/components/Misc/SocMediaIcons.jsx
@@ -0,0 +1,24 @@
+const SocMediaIcons = () => {
+  return (
+    <div>
+      <a target="_blank" href="#" aria-label="Facebook" className="soc-icon-link">
+        <i class="fa-brands fa-facebook-f fa-lg"></i>
+      </a>
+      <a target="_blank" href="#" aria-label="Twitter" className="soc-icon-link">
+        <i class="fa-brands fa-twitter fa-lg"></i>
+      </a>
+      <a target="_blank" href="#" aria-label="Instagram" className="soc-icon-link">
+        <i class="fa-brands fa-instagram fa-lg"></i>
+      </a>
+      <a
+        target="_blank"
+        href="https://github.com/kiknalex/"
+        aria-label="Github"
+       className="soc-icon-link">
+        <i class="fa-brands fa-github fa-lg"></i>
+      </a>
+    </div>
+  );
+};
+
+export default SocMediaIcons;
