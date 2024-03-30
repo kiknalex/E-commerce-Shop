b806a8a82f7a03df17c185fd3e052f7e6f3f0fb8
Alex <aleksandrekiknadzee@gmail.com>
2024-03-30 13:12:27 +0400

    fix: PickCategorySquare extra 20px background

--- src/components/Home/CategorySquares/PickCategorySquare.jsx
+++ src/components/Home/CategorySquares/PickCategorySquare.jsx
@@ -2,13 +2,15 @@ import CategorySquare from "./CategorySquare";
 
 const PickCategorySquare = () => {
   return (
-    <div className="container category-squares-container">
-      <h2 className="category-heading title">Pick featured collections</h2>
-      <div className="flex-category-squares">
-        <CategorySquare category={"Casual"} imgSrc={"/casual.jpg"} />
-        <CategorySquare category={"Formal"} imgSrc={"/formal.jpg"} />
-        <CategorySquare category={"Party"} imgSrc={"/party.jpg"} />
-        <CategorySquare category={"Gym"} imgSrc={"/gym.jpg"} />
+    <div className="container">
+      <div className="category-squares-container">
+        <h2 className="category-heading title">Pick featured collections</h2>
+        <div className="flex-category-squares">
+          <CategorySquare category={"Casual"} imgSrc={"/casual.jpg"} />
+          <CategorySquare category={"Formal"} imgSrc={"/formal.jpg"} />
+          <CategorySquare category={"Party"} imgSrc={"/party.jpg"} />
+          <CategorySquare category={"Gym"} imgSrc={"/gym.jpg"} />
+        </div>
       </div>
     </div>
   );
