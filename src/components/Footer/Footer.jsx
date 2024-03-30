49895e943f2b0de7ddfbd0fb4a543525bf8d6539
Alex <aleksandrekiknadzee@gmail.com>
2024-03-30 11:54:38 +0400

    feat: add appropriate fonts

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,93 +1,98 @@
 import SocMediaIcons from "../Misc/SocMediaIcons";
+import FooterSubscribe from "./FooterSubscribe";
 
 const Footer = () => {
   return (
-    <footer className="footer-container">
-      <div className="container">
-        <div className="flex-footer">
-          <div className="description-container">
-            <h1 className="title">
-              <a href="#">SHOP.CO</a>
-            </h1>
-            <p className="text--gray">
-              We have clothes that suits your style and which you’re proud to
-              wear. From women to men.
-            </p>
-            <SocMediaIcons />
-          </div>
-          <div className="footer-links">
-            <div className="links-container">
-              <h3 className="text text--bold">Company</h3>
-              <ul className="links-list text--gray">
-                <li>
-                  <a href="#">About</a>
-                </li>
-                <li>
-                  <a href="#">Features</a>
-                </li>
-                <li>
-                  <a href="#">Works</a>
-                </li>
-                <li>
-                  <a href="#">Career</a>
-                </li>
-              </ul>
+    <>
+      
+      <footer className="footer-container">
+      <FooterSubscribe />
+        <div className="container">
+          <div className="flex-footer">
+            <div className="description-container">
+              <h1 className="title">
+                <a href="#">SHOP.CO</a>
+              </h1>
+              <p className="text--gray">
+                We have clothes that suits your style and which you’re proud to
+                wear. From women to men.
+              </p>
+              <SocMediaIcons />
             </div>
-            <div className="links-container">
-              <h3 className="text text--bold">Help</h3>
-              <ul className="links-list text--gray">
-                <li>
-                  <a href="#">Customer Support</a>
-                </li>
-                <li>
-                  <a href="#">Delivery Details</a>
-                </li>
-                <li>
-                  <a href="#">Terms & Conditions</a>
-                </li>
-                <li>
-                  <a href="#">Privacy Policy</a>
-                </li>
-              </ul>
-            </div>
-            <div className="links-container">
-              <h3 className="text text--bold">FAQ</h3>
-              <ul className="links-list text--gray">
-                <li>
-                  <a href="#">Account</a>
-                </li>
-                <li>
-                  <a href="#">Manage Deliveries</a>
-                </li>
-                <li>
-                  <a href="#">Orders</a>
-                </li>
-                <li>
-                  <a href="#">Payments</a>
-                </li>
-              </ul>
-            </div>
-            <div className="links-container">
-              <h3 className="text text--bold">Resources</h3>
-              <ul className="links-list text--gray">
-                <li>
-                  <a href="#">Free eBooks</a>
-                </li>
-                <li>
-                  <a href="#">Development tutorial</a>
-                </li>
-                <li>
-                  <a href="#">How to - Blog</a>
-                </li>
-                <li>
-                  <a href="#">Youtube Playlist</a>
-                </li>
-              </ul>
+            <div className="footer-links">
+              <div className="links-container">
+                <h3 className="text text--bold">Company</h3>
+                <ul className="links-list text--gray">
+                  <li>
+                    <a href="#">About</a>
+                  </li>
+                  <li>
+                    <a href="#">Features</a>
+                  </li>
+                  <li>
+                    <a href="#">Works</a>
+                  </li>
+                  <li>
+                    <a href="#">Career</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="links-container">
+                <h3 className="text text--bold">Help</h3>
+                <ul className="links-list text--gray">
+                  <li>
+                    <a href="#">Customer Support</a>
+                  </li>
+                  <li>
+                    <a href="#">Delivery Details</a>
+                  </li>
+                  <li>
+                    <a href="#">Terms & Conditions</a>
+                  </li>
+                  <li>
+                    <a href="#">Privacy Policy</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="links-container">
+                <h3 className="text text--bold">FAQ</h3>
+                <ul className="links-list text--gray">
+                  <li>
+                    <a href="#">Account</a>
+                  </li>
+                  <li>
+                    <a href="#">Manage Deliveries</a>
+                  </li>
+                  <li>
+                    <a href="#">Orders</a>
+                  </li>
+                  <li>
+                    <a href="#">Payments</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="links-container">
+                <h3 className="text text--bold">Resources</h3>
+                <ul className="links-list text--gray">
+                  <li>
+                    <a href="#">Free eBooks</a>
+                  </li>
+                  <li>
+                    <a href="#">Development tutorial</a>
+                  </li>
+                  <li>
+                    <a href="#">How to - Blog</a>
+                  </li>
+                  <li>
+                    <a href="#">Youtube Playlist</a>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </footer>
+      </footer>
+    </>
   );
 };
 
