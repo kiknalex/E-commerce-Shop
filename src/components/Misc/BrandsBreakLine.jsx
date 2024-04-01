cd423f21400d121f1aa0878d044193b0ec4f39b8
Alex <aleksandrekiknadzee@gmail.com>
2024-04-01 12:26:56 +0400

    feat: add responsiveness for BrandsBreakLine component

--- src/components/Misc/BrandsBreakLine.jsx
+++ src/components/Misc/BrandsBreakLine.jsx
@@ -3,7 +3,7 @@ const BrandsBreakLine = () => {
     return (
         <div className="flex-brands brand-breakline">
             <img className="img-brand" src="/versace.png" alt="" width="166" height="33" />
-            <img className="img-brand" src="/zara.png" alt="" width="91" height="38" />
+            <img className="img-brand" src="/zara.png" alt="" width="92" height="38" />
             <img className="img-brand" src="/gucci.png" alt="" width="156" height="36" />
             <img className="img-brand" src="/prada.png" alt="" width="194" height="32" />
             <img className="img-brand" src="/calvinklein.png" alt="" width="206" height="33"  />
