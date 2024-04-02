e631fc6685a1517a087dc73c4863f350efd62a75
Alex <aleksandrekiknadzee@gmail.com>
2024-04-02 12:21:53 +0400

    fix: fix classes to classNames in SocMediaIcons

--- src/components/Misc/SocMediaIcons.jsx
+++ src/components/Misc/SocMediaIcons.jsx
@@ -2,20 +2,20 @@ const SocMediaIcons = () => {
   return (
     <div>
       <a target="_blank" href="#" aria-label="Facebook" className="soc-icon-link">
-        <i class="fa-brands fa-facebook-f fa-lg"></i>
+        <i className="fa-brands fa-facebook-f fa-lg"></i>
       </a>
       <a target="_blank" href="#" aria-label="Twitter" className="soc-icon-link">
-        <i class="fa-brands fa-twitter fa-lg"></i>
+        <i className="fa-brands fa-twitter fa-lg"></i>
       </a>
       <a target="_blank" href="#" aria-label="Instagram" className="soc-icon-link">
-        <i class="fa-brands fa-instagram fa-lg"></i>
+        <i className="fa-brands fa-instagram fa-lg"></i>
       </a>
       <a
         target="_blank"
         href="https://github.com/kiknalex/"
         aria-label="Github"
        className="soc-icon-link">
-        <i class="fa-brands fa-github fa-lg"></i>
+        <i className="fa-brands fa-github fa-lg"></i>
       </a>
     </div>
   );
