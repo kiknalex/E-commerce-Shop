7d0fff67ddfbee56f15e0e1cd2fc88632655fbe2
Alex <aleksandrekiknadzee@gmail.com>
2024-04-05 11:45:21 +0400

    feat: add search modal for mobile

--- src/components/Header/SearchMobile.jsx
+++ src/components/Header/SearchMobile.jsx
@@ -1,12 +1,28 @@
-const SearchMobile = ({mobileSearchShow, onClick}) => {
-    return (
-        <div className={`mobile-search-container ${mobileSearchShow}`}>
-            <button onClick={onClick}>close</button>
-        <form action="">
-        
-        </form>
-        </div>
-    )
-}
+const SearchMobile = ({ mobileSearchShow, onClick }) => {
+  return (
+    <form action="" className={`mobile-search-container ${mobileSearchShow}`}>
+      <div className="mobile-search">
+        <label htmlFor="search-product" hidden>
+          Search for items
+        </label>
+        <button className="btn-search" aria-label="Search">
+          <img src="/search-btn.png" className="btn-img" alt="search" />
+        </button>
+        <input
+          type="text"
+          name="search-product"
+          id="search-product"
+          aria-label="Search Products"
+          placeholder="Search for products..."
+        />
+      </div>
+      <div className="mobile-search-close">
+        <button onClick={onClick} aria-label="close" type="button">
+        <i className="fa-solid fa-xmark fa-2xl"></i>
+        </button>
+      </div>
+    </form>
+  );
+};
 
 export default SearchMobile;
