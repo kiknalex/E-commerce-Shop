a7cc764d26e1a589644deb2b296c979079ac1564
Alex <aleksandrekiknadzee@gmail.com>
2024-04-06 12:21:09 +0400

    feat: style hamburger menu

--- src/components/Header/HamburgerMobile.jsx
+++ src/components/Header/HamburgerMobile.jsx
@@ -1,24 +1,35 @@
 import { useEffect } from "react";
 const HamburgerMobile = ({ mobileMenuShow, onClick, onPointerLeave }) => {
   return (
-    <><div onTouchStart={onClick} onClick={onClick} className={`outside-hamburger-click-close ${mobileMenuShow}`}></div>
-    <nav onPointerLeave={onPointerLeave} className={`mobile-hamburger-container ${mobileMenuShow}`}>
-    
-      <ul className="flex-categories-mobile text--xl">
-        <li>
-          <a href="#">Shop</a>
-        </li>
-        <li>
-          <a href="#">On Sale</a>
-        </li>
-        <li>
-          <a href="#">New Arrivals</a>
-        </li>
-        <li>
-          <a href="#">Brands</a>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <div
+        onTouchStart={onClick}
+        onClick={onClick}
+        className={`outside-hamburger-click-close ${mobileMenuShow}`}
+      ></div>
+
+      <nav
+        onPointerLeave={onPointerLeave}
+        className={`mobile-hamburger-container ${mobileMenuShow}`}
+      >
+        <button onClick={onClick} aria-label="close" type="button" className="btn-modal-close">
+          <i className="fa-solid fa-xmark fa-2xl"></i>
+        </button>
+        <ul className="flex-categories-mobile text--xl">
+          <li>
+            <a href="#">Shop</a>
+          </li>
+          <li>
+            <a href="#">On Sale</a>
+          </li>
+          <li>
+            <a href="#">New Arrivals</a>
+          </li>
+          <li>
+            <a href="#">Brands</a>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 };
