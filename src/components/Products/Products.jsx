3deb1ca3e0385765a6781f7db1385b734dc16bfe
Alex <aleksandrekiknadzee@gmail.com>
2024-04-15 12:48:16 +0400

    feat: add sort on products page

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -7,7 +7,7 @@ const Products = () => {
   const [price, setPrice] = useState({ min: 0, max: 1000 });
   const [size, setSize] = useState([]);
   const [filterOptions, setFilterOptions] = useState({ price, size });
-
+  const [isSortOpen, setIsSortOpen] = useState(false);
   useEffect(() => {
     fetch(`https://fakestoreapi.com/products?limit=9`)
       .then((response) => response.json())
@@ -46,13 +46,16 @@ const Products = () => {
 
   const handlePriceEnter = (min, max) => {
     if (min > 1000 || max > 1000) {
+      setPrice({ min: 950, max: 1000 });
+      return null;
+    }
+    if (min >= max) {
       return null;
     }
     setPrice({ min, max });
   };
 
   const handleSizeClick = (newSize) => {
-    console.log(size);
     if (size.includes(newSize)) {
       setSize(size.filter((size) => size !== newSize));
     } else {
@@ -69,16 +72,30 @@ const Products = () => {
       price: { ...price },
       size: [...size],
     });
-    console.log(products);
   };
-
+  const handleSortClick = (event) => {
+    if (event.currentTarget.value !== event.target.value) {
+      setIsSortOpen(false);
+    }
+    setIsSortOpen((isSortOpen) => !isSortOpen);
+  };
+  const handleSortMode = (sortFn) => {
+    setProducts((products) => products.toSorted(sortFn));
+    setIsSortOpen(false);
+  };
   return (
     <main className="">
       <div className="container products-container-grid">
         <div className="filters-container">
           <div className="filters-heading">
             <h2>Filters</h2>
-            <i>filter icon</i>
+            <button
+              disabled
+              aria-label="filter-disabled"
+              className="btn-search btn-uninteractive"
+            >
+              <img src="/filterIconDisabled.png" alt="" />
+            </button>
           </div>
           <div>
             <ul className="filter-categories">
@@ -99,19 +116,32 @@ const Products = () => {
               onInput={(value) => setPrice({ min: value[0], max: value[1] })}
             />
             <div className="filter-price-num">
+              <label htmlFor="min-price-input" className="sr-only">
+                Enter Minimum Price
+              </label>
               <input
                 type="text"
                 value={price.min}
+                autocomplete="off"
                 onChange={(e) => handlePriceEnter(e.target.value, price.max)}
                 inputmode="numeric"
                 className="filter-price-num-input"
+                maxLength={9}
+                placeholder="0"
+                id="min-price-input"
               />
+              <label htmlFor="max-price-input" className="sr-only">
+                Enter Maximum Price
+              </label>
               <input
                 type="text"
                 value={price.max}
+                autocomplete="off"
                 onChange={(e) => handlePriceEnter(price.min, e.target.value)}
                 inputmode="numeric"
                 className="filter-price-num-input"
+                maxLength={9}
+                id="max-price-input"
               />
             </div>
           </div>
@@ -154,9 +184,43 @@ const Products = () => {
         <div className="products-container">
           <div className="products-heading">
             <h1>Casual</h1>
-            <div>
+            <div className="products-sort">
               <p>showing 1-10 of 100 products</p>
-              <p>sort by</p>
+              <div
+                className="dropdown-sort-container"
+                onBlur={(e) =>
+                  !e.currentTarget.contains(e.relatedTarget) &&
+                  setIsSortOpen(false)
+                }
+              >
+                <button onClick={handleSortClick} className="btn-dropdown-sort">
+                  Sort By
+                </button>
+                <div className={`dropdown-sort ${isSortOpen ? "open" : ""}`}>
+                  <ul className="dropdown-sort-list">
+                    <li>
+                      <button
+                        className="btn-dropdown-sort"
+                        onClick={() =>
+                          handleSortMode((a, b) => a.price - b.price)
+                        }
+                      >
+                        Lowest Price
+                      </button>
+                    </li>
+                    <li>
+                      <button
+                        className="btn-dropdown-sort"
+                        onClick={() =>
+                          handleSortMode((a, b) => b.price - a.price)
+                        }
+                      >
+                        Highest Price
+                      </button>
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </div>
           </div>
           <div className="products-list">
