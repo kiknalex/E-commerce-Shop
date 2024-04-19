656bb52b6eb4054384d788c5327662760b6d5607
Alex <aleksandrekiknadzee@gmail.com>
2024-04-19 15:03:39 +0400

    feat: add product info mockup

--- src/components/Products/Product.jsx
+++ src/components/Products/Product.jsx
@@ -6,13 +6,26 @@ const Product = () => {
   const params = useParams();
   console.log(params.category);
   console.log(params.id);
-  useEffect(() => {
-    fetch(`fetch('https://fakestoreapi.com/products/${params.id}')`)
-  }, [])
+  //   useEffect(() => {
+  //     fetch(`https://fakestoreapi.com/products/${params.id}`)
+  //     .then(response => response.json())
+  //     .then(data => setProductDetails(data))
+  //     .catch(error => console.error(error));
+  //   }, [])
   return (
     <main>
       <Path />
-      <div className="product-container"></div>
+      <div className="product-container">
+        <div className="gallery">
+          <img src="" alt="" />
+          <div className="gallery-thumbnails"></div>
+        </div>
+        <div className="product-info">
+          <div className="product-description"></div>
+          <ul className="select-size-container"></ul>
+          <div className="add-cart"></div>
+        </div>
+      </div>
     </main>
   );
 };
