6145da5e521ff4e4bd1ddf9b0a6b9dd34374d3b8
Alex <aleksandrekiknadzee@gmail.com>
2024-04-26 11:48:05 +0400

    feat: add accessibility features to the FooterSubscribe

--- src/components/Footer/FooterSubscribe.jsx
+++ src/components/Footer/FooterSubscribe.jsx
@@ -1,14 +1,20 @@
 const FooterSubscribe = () => {
   return (
     <div className="container subscribe-container">
-      <h4 className="title subscribe-heading">Stay upto date about our latest offers</h4>
-      <form action="submit" className="subscribe-form">
+      <h4 className="title subscribe-heading">
+        Stay upto date about our latest offers
+      </h4>
+      <form className="subscribe-form" aria-label="Subscribe to newsletter">
         <input
           type="email"
-          className="subscribe-input btn-pill"
+          className="subscribe-input btn-pill subscribe-input-email"
           placeholder="Enter your email address..."
+          aria-label="Enter your email address"
         />
-        <button className="btn-pill subscribe-input">Subscribe to newsletter</button>
+
+        <button className="btn-pill subscribe-input">
+          Subscribe to newsletter
+        </button>
       </form>
     </div>
   );
--- src/components/Misc/PageNotFound.jsx
+++ src/components/Misc/PageNotFound.jsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
 const PageNotFound = () => {
   return (
-    <main>
-      <h1>404 Page not found</h1>
+    <main className="container">
+      <h1 className="title">404 Page not found</h1>
       <Link to="/" className="title">Go back to Home</Link>
     </main>
   );
