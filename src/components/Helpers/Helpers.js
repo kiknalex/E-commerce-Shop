7c3eb54e8ace05942444889b22ad913851a58704
Alex <aleksandrekiknadzee@gmail.com>
2024-05-01 12:31:08 +0400

    feat: add price calculation helpers

--- src/components/Helpers/Helpers.js
+++ src/components/Helpers/Helpers.js
@@ -1,5 +1,18 @@
 const capitalize = (word) => {
     return word.charAt(0).toUpperCase() + word.slice(1);
 }
+const calculateSubtotal = (cart) => {
+   return cart.reduce((acc, item) => {
+        return acc + item.price;
+    }, 0)
+}
 
-export {capitalize};
+const calculateTotal = (cart, discount, fee) => {
+    let finalPrice = 0;
+    finalPrice = calculateSubtotal(cart);
+    finalPrice -= (finalPrice / 100 * discount);
+    finalPrice -= fee;
+    return finalPrice;
+}
+
+export {capitalize, calculateSubtotal, calculateTotal};
