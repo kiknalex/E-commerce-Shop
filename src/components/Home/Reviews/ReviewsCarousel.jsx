28badfd17d918aa146778e56dd07e209083b9271
Alex <aleksandrekiknadzee@gmail.com>
2024-05-06 14:10:02 +0400

    fix: incorrect usage of Object.keys function

--- src/components/Home/Reviews/ReviewsCarousel.jsx
+++ src/components/Home/Reviews/ReviewsCarousel.jsx
@@ -10,18 +10,17 @@ import useEmblaCarousel from "embla-carousel-react";
 const ReviewsCarousel = () => {
   const [emblaRef, emblaApi] = useEmblaCarousel();
   const [fakeReviews, setFakeReviews] = useState([]);
+
   useEffect(() => {
-    setFakeReviews([
-      ...Array(10)
-        .keys()
-        .map((el) => {
-          return {
-            firstName: faker.person.firstName(),
-            lastNameLetter: faker.person.lastName()[0],
-            text: faker.lorem.words(10),
-          };
-        }),
-    ]);
+    setFakeReviews(
+      Object.keys([...Array(10)]).map((el) => {
+        return {
+          firstName: faker.person.firstName(),
+          lastNameLetter: faker.person.lastName()[0],
+          text: faker.lorem.words(10),
+        };
+      })
+    );
   }, []);
 
   return (
@@ -30,11 +29,11 @@ const ReviewsCarousel = () => {
         <h2 className="heading-carousel title">Our Happy Customers</h2>
         <div className="slider-buttons">
           <PrevButton
-          aria-label="Previous review"
+            aria-label="Previous review"
             onClick={usePrevNextButtons(emblaApi).onPrevButtonClick}
           />
           <NextButton
-          aria-label="Next review"
+            aria-label="Next review"
             onClick={usePrevNextButtons(emblaApi).onNextButtonClick}
           />
         </div>
@@ -43,12 +42,12 @@ const ReviewsCarousel = () => {
         <div className="embla__container">
           {fakeReviews.map((review, id) => (
             <div className="review-wrapper" key={id}>
-            <ReviewCard
-              className="embla__slide"
-              firstName={review.firstName}
-              lastNameLetter={review.lastNameLetter}
-              text={review.text}
-            />
+              <ReviewCard
+                className="embla__slide"
+                firstName={review.firstName}
+                lastNameLetter={review.lastNameLetter}
+                text={review.text}
+              />
             </div>
           ))}
         </div>
