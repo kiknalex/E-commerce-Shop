5a572a3469d11e83a86e0ce7911e90c261a29e48
Alex <aleksandrekiknadzee@gmail.com>
2024-05-07 15:59:52 +0400

    fix: adjust price calculations to decimals

--- src/components/Helpers/Helpers.js
+++ src/components/Helpers/Helpers.js
@@ -1,21 +1,25 @@
 const capitalize = (word) => {
   return word.charAt(0).toUpperCase() + word.slice(1);
 };
+
 const calculateSubtotal = (cart) => {
-  return cart.reduce((acc, item) => {
-    return Math.round(acc + item.price * item.quantity);
-  }, 0);
+  return cart
+    .reduce((acc, item) => {
+      return acc + item.price * item.quantity;
+    }, 0)
+    .toFixed(2);
 };
 
 const calculateDiscount = (cart, discount) => {
   return Math.round((calculateSubtotal(cart) / 100) * discount);
 };
+
 const calculateTotal = (cart, discount, fee) => {
   let finalPrice = 0;
   finalPrice = calculateSubtotal(cart);
   finalPrice -= calculateDiscount(cart, discount);
-  finalPrice -= fee;
-  return Math.round(finalPrice);
+  finalPrice += fee;
+  return finalPrice.toFixed(2);
 };
 
 export { capitalize, calculateSubtotal, calculateDiscount, calculateTotal };
