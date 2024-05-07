a7fa2a2f264f5788aca170c20ddd3d6beb6e16fe
Alex <aleksandrekiknadzee@gmail.com>
2024-05-07 11:08:05 +0400

    feat: add real links to the Social Media Icons in footer

--- src/components/Misc/SocMediaIcons.jsx
+++ src/components/Misc/SocMediaIcons.jsx
@@ -1,15 +1,6 @@
 const SocMediaIcons = () => {
   return (
     <div>
-      <a target="_blank" href="#" aria-label="Facebook" className="soc-icon-link">
-        <i className="fa-brands fa-facebook-f fa-lg"></i>
-      </a>
-      <a target="_blank" href="#" aria-label="Twitter" className="soc-icon-link">
-        <i className="fa-brands fa-twitter fa-lg"></i>
-      </a>
-      <a target="_blank" href="#" aria-label="Instagram" className="soc-icon-link">
-        <i className="fa-brands fa-instagram fa-lg"></i>
-      </a>
       <a
         target="_blank"
         href="https://github.com/kiknalex/"
@@ -17,6 +8,16 @@ const SocMediaIcons = () => {
        className="soc-icon-link">
         <i className="fa-brands fa-github fa-lg"></i>
       </a>
+      <a target="_blank" href="https://www.facebook.com/" aria-label="Facebook" className="soc-icon-link">
+        <i className="fa-brands fa-facebook-f fa-lg"></i>
+      </a>
+      <a target="_blank" href="https://twitter.com/" aria-label="Twitter" className="soc-icon-link">
+        <i className="fa-brands fa-twitter fa-lg"></i>
+      </a>
+      <a target="_blank" href="https://www.instagram.com/" aria-label="Instagram" className="soc-icon-link">
+        <i className="fa-brands fa-instagram fa-lg"></i>
+      </a>
+      
     </div>
   );
 };
